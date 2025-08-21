87d3154de26cc9562d6f2310b2e5d719edd7efe2
rahulstackdecode <rahulstackdecode@gmail.com>
2025-08-21 18:50:07 +0530

    Css Updates

--- src/app/components/Logo-Icon.jsx
+++ src/app/components/Logo-Icon.jsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function LogoIcon({ className }) {
     return (
         <div className={`logo-wrapper flex items-center ${className}`}>
             <Link href="/">
-                <Image
-                    src="/icon-logo.png"
-                    alt="Icon Logo"
-                    width={30} height={30}
-                    priority
-                />
+               <img src="/icon-logo.png" alt="Icon Logo" width={30} height={30} />
+
             </Link>
         </div>
     );
