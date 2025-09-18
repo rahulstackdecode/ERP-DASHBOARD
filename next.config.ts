9dc9fbb786c669d69beca8c267d06a50f61f3efd
rahulstackdecode <rahulstackdecode@gmail.com>
2025-09-18 19:35:38 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "mtidyqxczgofjeuvrtcm.supabase.co"
-    ],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
